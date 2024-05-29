0132eb6a85bbf5425c51aec0996b85eb97f937c6
WaliSiddiqui1 <160671964+WaliSiddiqui1@users.noreply.github.com>
2024-05-28 21:28:18 -0400

    Update Wind_velocity_Isolation.py

--- Wind_velocity_Isolation.py
+++ Wind_velocity_Isolation.py
@@ -14,23 +14,26 @@ import pyproj
 import proplot as pplt
 import math
 
+#Filtering the data for the date and speed
 file = '/Users/saminakashif/Downloads/ASFS30UCB2.csv'
 data = pd.read_csv(file)
 selected_columns = data[['datetime', 'u_wind', 'v_wind', 'speed']]
 selected_columns.to_csv('wind_speed.csv', index=False)
 speed_data = pd.read_csv('wind_speed.csv')
 
+#Calcuating the speed of the wind and inputting into the dataframe
 speed_data['wind_velocity'] = np.sqrt(speed_data['u_wind'] ** 2 + speed_data['v_wind'] ** 2)
 speed_data['wind_velocity'] = speed_data['wind_velocity'].fillna(0)
 
+#Calculating the angle of the data and putting it into the dataframe
 speed_data['wind_angle'] = np.arcsin(speed_data['u_wind'] / speed_data['v_wind'])
 speed_data['wind_angle'] = speed_data['wind_angle'].fillna(0) 
-
 speed_data['sin_angle'] = np.sin(speed_data['wind_angle'])
 speed_data['sin_angle'] = speed_data['sin_angle'].fillna(0)
 
 speed_data.to_csv('wind_speed.csv', index=False)
 
+#Alpha value for the ice velocity calculation
 alpha = 0.015
 
 # Converting velocity into a 1D complex number shortens the number of steps
@@ -49,6 +52,7 @@ speed_data.to_csv('wind_speed.csv', index=False)
 
 print(speed_data)
 
+#Creating graph that plots the datetime against the calculated velocity to show the progression over time
 fig, axs = pplt.subplots(width=7, height=5, nrows=1, spany=False)
 for ax, dir in zip(axs, ['real_speed', 'speed']):
     ax.plot(speed_data['datetime'], speed_data['real_speed'], label='Estimate')
