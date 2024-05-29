c4341be5836b841f41ae46d8972e22576cd5f855
WaliSiddiqui1 <160671964+WaliSiddiqui1@users.noreply.github.com>
2024-05-28 21:23:38 -0400

    Update Turning-angle.py

--- Turning-angle.py
+++ Turning-angle.py
@@ -13,7 +13,7 @@ import pyproj
 import proplot as pplt
 import math
 
-
+#Accessing the data files for asfs30 data
 DF1 = '/Users/saminakashif/Downloads/asfs30.csv'
 DF2 = '/Users/saminakashif/Downloads/ASFS30UCB2.csv'
 DF3 = '/Users/saminakashif/Downloads/ASFS30UCB3.csv'
@@ -26,15 +26,18 @@ Data3 = pd.read_csv(DF3)
 Data4 = pd.read_csv(DF4)
 Data5 = pd.read_csv(DF5)
 
+#Filtering the data for just the date and speed data
 selected_columns = Data5[['datetime', 'u_wind', 'v_wind', 'speed']]
 selected_columns.to_csv('wind_speed.csv', index=False)
 speed_data2 = pd.read_csv('wind_speed.csv')
 
+#Calculating the angles of the data
 speed_data2['wind_angle'] = np.arctan(speed_data2['u_wind'] / speed_data2['v_wind'])
 speed_data2['wind_angle'] = speed_data2['wind_angle'].fillna(0) 
 
 print (speed_data2)
 
+#Creating a plot of the turning angles and how they progressed over the time frame of the data
 fig, axs = pplt.subplots(width=7, height=5, nrows=1, spany=False)
 for ax, dir in zip(axs, ['wind_angle']):
     ax.plot(speed_data2['datetime'], speed_data2['wind_angle'], label='Estimate')
@@ -42,5 +45,3 @@ for ax, dir in zip(axs, ['wind_angle']):
     step = len(speed_data2) // 10
     ax.format(xticks=step)
 ax.legend(loc='ll', ncols=1)
-
-
